56f0a89ae190d3700b20cc1c2d82c42766f8baed
Jared Krinke <jared.krinke@gmail.com>
2020-02-17 19:29:37 -0800

    Verify solutions on upload

--- sic1/server/src/api.ts
+++ sic1/server/src/api.ts
@@ -7,7 +7,10 @@ import * as Validize from "validize";
 import * as Contract from "sic1-server-contract";
 import * as Firebase from "firebase-admin";
 import * as fbc from "fbc";
-import { puzzles } from "sic1-shared";
+import { Puzzle, shuffleInPlace, generatePuzzleTest, puzzles } from "sic1-shared";
+import { AssembledProgram, Emulator } from "sic1asm";
+
+const identity = <T extends unknown>(x: T) => x;
 
 // Puzzle list
 const puzzleSet: {[title: string]: boolean} = {};
@@ -179,6 +182,90 @@ function createSolutionDocumentFromSolution(solution: Solution): SolutionDocumen
     };
 }
 
+// Solution validation
+function getPuzzle(title: string): Puzzle {
+    for (const group of puzzles) {
+        for (const puzzle of group.list) {
+            if (puzzle.title === title) {
+                return puzzle;
+            }
+        }
+    }
+
+    throw new Validize.ValidationError(`Test not found: ${title}`);
+}
+
+function verifyProgram(inputs: number[], expectedOutputs: number[], program: AssembledProgram, maxCyclesExecuted: number, maxMemoryBytesAccessed: number): void {
+    let inputIndex = 0;
+    let outputIndex = 0;
+
+    let correct = true;
+    const emulator = new Emulator(program, {
+        readInput: () => inputs[inputIndex++],
+        writeOutput: n => {
+            const expected = expectedOutputs[outputIndex++];
+            if (n !== expected) {
+                correct = false;
+            }
+        },
+    });
+
+    while (correct && outputIndex < expectedOutputs.length && emulator.getCyclesExecuted() <= maxCyclesExecuted && emulator.getMemoryBytesAccessed() <= maxMemoryBytesAccessed) {
+        emulator.step();
+    }
+
+    if (emulator.getCyclesExecuted() > maxCyclesExecuted || emulator.getMemoryBytesAccessed() > maxMemoryBytesAccessed) {
+        throw new Validize.ValidationError(`Execution did not complete within ${maxCyclesExecuted} cycles and ${maxMemoryBytesAccessed} bytes`);
+    }
+
+    if (!correct) {
+        throw new Validize.ValidationError("Incorrect output produced");
+    }
+}
+
+const verificationMaxCycles = 100000;
+function verifySolution(solution: Solution): void {
+    const puzzle = getPuzzle(solution.testName);
+    const test = generatePuzzleTest(puzzle);
+    const bytes: number[] = [];
+    for (let i = 0; i < solution.program.length; i += 2) {
+        bytes.push(parseInt(solution.program.substr(i, 2), 16));
+    }
+
+    const program: AssembledProgram = {
+        bytes,
+        sourceMap: [],
+        variables: [],
+    };
+
+    // Verify using standard input and supplied stats
+    verifyProgram(test.testSets[0].input, test.testSets[0].output, program, solution.cyclesExecuted, solution.memoryBytesAccessed);
+
+    // Verify using shuffled standard input (note: this ensures the order is different)
+    const shuffeldStandardIO = puzzle.io.slice();
+    const originalFirst = shuffeldStandardIO[0];
+    shuffleInPlace(shuffeldStandardIO);
+
+    // Ensure not idential to standard
+    if (originalFirst === shuffeldStandardIO[0] && shuffeldStandardIO.length > 1) {
+        shuffeldStandardIO[0] = shuffeldStandardIO[1];
+        shuffeldStandardIO[1] = originalFirst;
+    }
+
+    verifyProgram(
+        identity<number[]>([]).concat(...shuffeldStandardIO.map(a => a[0])),
+        identity<number[]>([]).concat(...shuffeldStandardIO.map(a => a[1])),
+        program,
+        verificationMaxCycles,
+        solutionBytesMax
+    );
+
+    if (puzzle.test) {
+        // Verify using random input
+        verifyProgram(test.testSets[1].input, test.testSets[1].output, program, verificationMaxCycles, solutionBytesMax);
+    }
+}
+
 function hasProperties(o: object): boolean {
     for (let key in o) {
         return true;
@@ -236,13 +323,11 @@ async function updateSolutionAndAggregations(reference: Firebase.firestore.Docum
 
 async function addSolution(solution: Solution, context: Koa.Context): Promise<void> {
     try {
-        // Only allow valid puzzles
-        const { userId, testName } = solution;
-        if (puzzleSet[testName] !== true) {
-            throw new Validize.ValidationError(`Invalid test name: ${testName}`);
-        }
+        // Verify the solution and stats first
+        verifySolution(solution);
 
         // Check to see if the user already solved this puzzle
+        const { userId, testName } = solution;
         const cyclesFocusedSolutionReference = root.doc(createSolutionDocumentId(userId, testName, SolutionFocus.cyclesExecuted));
         const bytesFocusedSolutionReference = root.doc(createSolutionDocumentId(userId, testName, SolutionFocus.memoryBytesAccessed));
 
