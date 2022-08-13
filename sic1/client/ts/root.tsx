07c872638a53e2d7d7e105eef1f2426b6b7f57a8
Jared Krinke <jared.krinke@gmail.com>
2022-08-13 14:25:13 -0700

    Update promotion messages

--- sic1/client/ts/root.tsx
+++ sic1/client/ts/root.tsx
@@ -178,28 +178,29 @@ export class Sic1Root extends React.Component<{}, Sic1RootState> {
 
         // Senior Engineer
         data => <><p>Impressive work, {data.name}! Based on your stellar performance, I'm promoting you to {Sic1Root.getJobTitle(data)}.</p><p>Your next couple of assignments are
-            very important (and difficult), so please get started as soon as you can. Thanks for taking the time to prioritize this work over competing priorities in your personal
+            very important (and difficult), so please get started as soon as you can. Thanks for taking the time to prioritize this work over competing demands in your personal
             life!</p></>,
 
         // Principal Engineer
         data => <><p>Spectacular work, {data.name}! Based on your innovative solutions, you are being promoted to {Sic1Root.getJobTitle(data)}.</p><p>Your new assignments are on
             the bleeding edge of SIC Systems research. Welcome to the exciting world of natural language processing! As always, we greatly appreciate your willingness to work
-            night and day to make SIC Systems profitable! Even though it's getting late, if you could continue your work, that would be super helpful. Thanks!</p></>,
+            night and day to make SIC Systems more profitable! Even though it's getting late in the day, if you could continue your work, that would be super helpful.
+            Thanks!</p></>,
 
         // Partner Engineer
         data => <><p>Incredible work, {data.name}! After consulting with the SIC Systems board, I've been given special permission to promote you to {Sic1Root.getJobTitle(data)}.
             </p><p>You've shown tenacity to get this far, and you'll need loads of it for the next batch of tasks. We need to give the SIC-1 the ability to understand its own
             code, in order to unleash its immense computing power on optimizing its own performance. We'll be happy to provide on-site food and laundry service, home
-            cleaning/maintenance, child care, and anti-aging services to you as part of your compensation package. We just need you to push through this one last sprint to
+            cleaning/maintenance, and fertility preservation services to you as part of your compensation package. We just need you to push through this one last sprint to
             the finish line. Your fellow SIC Systems family members thank you for your perseverance!</p></>,
 
         // Technical Fellow Emeritus
         data => <><p>Truly amazing work, {data.name}! The SIC Systems board unanimously voted to create a new title just for you: {Sic1Root.getJobTitle(data)}.</p><p>Thank you
-            from the bottom of my heart for all of the sacrifices you've made to get us to this point. The SIC-1 is now able to create and reason about its own code. This is an
-            amazing breakthrough and you should be very proud.</p><p>Now that we've reached this exciting milestone (thanks to your tireless efforts!), SIC Systems really can't
-            challenge someone of your vast talents. You can now begin the next phase of your career at one of the many other technology companies around the world. I know
-            parting ways is tough, but SIC Systems is a business, not a family, so we have to say goodbye to employees once they're no longer needed. Thanks one last time,
-            and best of luck to you!</p></>,
+            from the bottom of my heart for all of the sacrifices you've made to get us to this point. The SIC-1 is now able to reason about its own code. This is an
+            amazing breakthrough and you should be very proud.</p><p>Now that we've reached this exciting milestone (thanks to your tireless efforts!), SIC Systems honestly can't
+            challenge someone with your peerless talent. Excitingly, you can now begin the next phase of your career at one of the many other technology companies around the world.
+            I know parting ways is tough, but SIC Systems is a business, not a family, so we have to say goodbye to employees once they're no longer needed. Thank you one last
+            time, and best of luck in your future endeavors!</p></>,
     ];
 
     private ide = React.createRef<Sic1Ide>();
@@ -250,6 +251,7 @@ export class Sic1Root extends React.Component<{}, Sic1RootState> {
     }
 
     private static getPromotionMessage(data: UserData): React.ReactFragment {
+        // TODO: Is this the same as promotionMessages[0]? Can that be used here instead?
         let message: React.ReactFragment = <><p>Congratulations, {data.name}! In recognition of your contributions to SIC Systems, you have been promoted to {Sic1Root.getJobTitle(data)}.</p></>;
         for (let i = 0; i < Sic1Root.jobTitles.length; i++) {
             const row = Sic1Root.jobTitles[i];
