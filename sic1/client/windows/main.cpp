bc64aee6504b4132c5b4a6a383ede44ad8524e5b
Jared Krinke <jared.krinke@gmail.com>
2022-08-29 11:40:28 -0700

    #61 WebView2 build for Windows

---
+++ sic1/client/windows/main.cpp
@@ -0,0 +1,107 @@
+#include <windows.h>
+#include <tchar.h>
+#include <wrl.h>
+#include <wil/com.h>
+#include "WebView2.h"
+
+#define SIC1_DOMAIN L"sic1-assets.schemescape.com"
+#define SIC1_ROOT L"https://" SIC1_DOMAIN L"/index.html"
+
+using namespace Microsoft::WRL;
+using namespace wil;
+
+static TCHAR szWindowClass[] = _T("DesktopApp");
+static TCHAR szTitle[] = _T("SIC-1");
+static com_ptr<ICoreWebView2Controller> webviewController;
+static com_ptr<ICoreWebView2> webviewWindow;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+	// TODO: Less obnoxious error handling
+	if (!RegisterClassEx(&wcex)) {
+		MessageBox(NULL, _T("Call to RegisterClassEx failed!"), szTitle, NULL);
+		return 1;
+	}
+
+	HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1200, 900, NULL, NULL, hInstance, NULL);
+
+	if (!hWnd) {
+		MessageBox(NULL, _T("Call to CreateWindow failed!"), szTitle, NULL);
+		return 1;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+		Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+			[hWnd](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+				env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+					[hWnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+						if (controller != nullptr) {
+							webviewController = controller;
+							webviewController->get_CoreWebView2(&webviewWindow);
+						}
+
+						RECT bounds;
+						GetClientRect(hWnd, &bounds);
+						webviewController->put_Bounds(bounds);
+
+						// Reference SIC-1 assets
+						auto webView3 = webviewWindow.query<ICoreWebView2_3>();
+						webView3->SetVirtualHostNameToFolderMapping(SIC1_DOMAIN, L"assets", COREWEBVIEW2_HOST_RESOURCE_ACCESS_KIND_ALLOW);
+
+						// Initial navigation
+						webviewWindow->Navigate(SIC1_ROOT);
+
+						return S_OK;
+					}).Get());
+				return S_OK;
+			}).Get());
+
+	// Main message loop:
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+		case WM_SIZE:
+			if (webviewController != nullptr) {
+				RECT bounds;
+				GetClientRect(hWnd, &bounds);
+				webviewController->put_Bounds(bounds);
+			};
+			break;
+
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+			break;
+	}
+
+	return 0;
+}
