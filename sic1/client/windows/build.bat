ca7ff7e87e6a76098270319210bdd0f17df8ec43
Jared Krinke <jared.krinke@gmail.com>
2022-08-30 14:06:58 -0700

    Consistent timestamps for build archives

--- sic1/client/windows/build.bat
+++ sic1/client/windows/build.bat
@@ -1,5 +1,9 @@
+mkdir builds
+
+for /f %%i in ('PowerShell Get-Date -Format FileDateTimeUniversal') do (
 msbuild sic1.sln /p:Configuration=Release /t:rebuild /p:Platform=x86
-powershell Compress-Archive -Path Release\assets,Release\sic1.exe,Release\WebView2Loader.dll -DestinationPath "sic1-x86-$(Get-Date -Format FileDateTimeUniversal).zip"
+powershell Compress-Archive -Path Release\assets,Release\sic1.exe,Release\WebView2Loader.dll -DestinationPath "builds\\sic1-%%i-x86.zip"
 
 msbuild sic1.sln /p:Configuration=Release /t:rebuild /p:Platform=x64
-powershell Compress-Archive -Path x64\Release\assets,x64\Release\sic1.exe,x64\Release\WebView2Loader.dll -DestinationPath "sic1-x64-$(Get-Date -Format FileDateTimeUniversal).zip"
+powershell Compress-Archive -Path x64\Release\assets,x64\Release\sic1.exe,x64\Release\WebView2Loader.dll -DestinationPath "builds\\sic1-%%i-x64.zip"
+)
