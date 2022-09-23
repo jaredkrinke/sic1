56d7e0a5117c1b40c2cc1cab2a3bd3af9b2b22ea
Jared Krinke <jared.krinke@gmail.com>
2022-09-23 15:35:12 -0700

    #111 Remove unnecessary links in emails

--- sic1/client/ts/mail.tsx
+++ sic1/client/ts/mail.tsx
@@ -2,7 +2,6 @@ import { ComponentChildren } from "preact";
 import { Puzzle, puzzleFlatArray } from "sic1-shared";
 import { Shared } from "./shared";
 import { Sic1DataManager } from "./data-manager";
-import { TextButton } from "./text-button";
 import { createPuzzleCharts } from "./puzzle-list";
 
 export interface Contact {
@@ -31,48 +30,20 @@ const manager = {
     title: "Director of Engineering",
 };
 
-function createPromotionMessage(solvedCount: number, subject: string, create: (self: Contact, callbacks: MailCallbacks) => ComponentChildren): MailData {
-    let puzzleLinkText: string;
-    let assignmentText: string;
-    switch (solvedCount) {
-        case 0:
-            puzzleLinkText = "Introduction to Subleq";
-            assignmentText = "first";
-            break;
-
-        case puzzleFlatArray.length:
-            break;
-
-        default:
-            puzzleLinkText = puzzleFlatArray[solvedCount].title;
-            assignmentText = "next";
-            break;
-    }
-
+function createPromotionMessage(subject: string, create: (self: Contact, callbacks: MailCallbacks) => ComponentChildren): MailData {
     return {
         subject,
         from: manager,
         create: (self: Contact, callbacks: MailCallbacks) => <>
             {create(self, callbacks)}
             <p>-{managerFirstName}</p>
-            {puzzleLinkText
-                ? <>
-                    <br />
-                    <p>Click to proceed to your {assignmentText} assignment:</p>
-                    <p>&gt; <TextButton text={puzzleLinkText} onClick={() => {
-                        // Note: this clears all message boxes, including this one
-                        callbacks.onLoadPuzzleRequested(puzzleFlatArray[solvedCount]);
-                    }} /></p>
-                </>
-                : <></>
-            }
         </>,
     };
 }
 
 const solvedMails: (MailData[] | null)[] = [
     // Trainee
-    [createPromotionMessage(Shared.jobTitles[0].minimumSolved, "Welcome to the team!", (self, callbacks) => <>
+    [createPromotionMessage("Welcome to the team!", (self, callbacks) => <>
         <p>Congratulations, {self.name}! SIC Systems has accepted your application. Introductory information and your first assignment are below.</p>
         <p>Introducing the SIC-1</p>
         <p>The SIC-1 represents a transformational change in computing, reducing complexity to the point that the processor only executes a single instruction: subtract and branch if less than or equal to zero ("subleq").</p>
@@ -84,7 +55,7 @@ const solvedMails: (MailData[] | null)[] = [
     // Data Directive and Looping
     null,
     // First Assessment // Engineer
-    [createPromotionMessage(Shared.jobTitles[1].minimumSolved, "Training complete!", (self) => <><p>Thanks for completing your introductory training assignments, {self.name}! Your starting job title is: {Shared.jobTitles[1].title}.</p><p>Please get started
+    [createPromotionMessage("Training complete!", (self) => <><p>Thanks for completing your introductory training assignments, {self.name}! Your starting job title is: {Shared.jobTitles[1].title}.</p><p>Please get started
         on your assignments right away.</p></>)],
 
     // Addition
@@ -96,7 +67,7 @@ const solvedMails: (MailData[] | null)[] = [
     // Multiplication
     null,
     // Division // Engineer II
-    [createPromotionMessage(Shared.jobTitles[2].minimumSolved, "Great start!", (self) => <><p>Nice work on the arithmetic programs, {self.name}! As of this email, you have been promoted to {Shared.jobTitles[2].title}.</p><p>Please continue your work.
+    [createPromotionMessage("Great start!", (self) => <><p>Nice work on the arithmetic programs, {self.name}! As of this email, you have been promoted to {Shared.jobTitles[2].title}.</p><p>Please continue your work.
         We expect great things from you!</p></>)],
 
     // Sequence Sum
@@ -104,7 +75,7 @@ const solvedMails: (MailData[] | null)[] = [
     // Sequence Cardinality
     null,
     // Number to Sequence // Senior Engineer
-    [createPromotionMessage(Shared.jobTitles[3].minimumSolved, "Excellent work!", (self) => <><p>Impressive work, {self.name}! Based on your stellar performance, I'm promoting you to {Shared.jobTitles[3].title}.</p><p>Your next couple of assignments are
+    [createPromotionMessage("Excellent work!", (self) => <><p>Impressive work, {self.name}! Based on your stellar performance, I'm promoting you to {Shared.jobTitles[3].title}.</p><p>Your next couple of assignments are
         very important (and difficult), so please get started as soon as you can. Thanks for taking the time to prioritize this work over competing demands in your personal
         life!</p></>)],
 
@@ -122,7 +93,7 @@ const solvedMails: (MailData[] | null)[] = [
     // Sort
     null,
     // Mode // Principal Engineer
-    [createPromotionMessage(Shared.jobTitles[4].minimumSolved, "A well-deserved promotion", (self) => <><p>Spectacular work, {self.name}! Based on your innovative solutions, you are being promoted to {Shared.jobTitles[4].title}.</p><p>Your new assignments are on
+    [createPromotionMessage("A well-deserved promotion", (self) => <><p>Spectacular work, {self.name}! Based on your innovative solutions, you are being promoted to {Shared.jobTitles[4].title}.</p><p>Your new assignments are on
         the bleeding edge of SIC Systems research. Welcome to the exciting world of natural language processing! As always, we greatly appreciate your willingness to work
         night and day to make SIC Systems more profitable! Even though it's getting late in the day, if you could continue your work, that would be super helpful.
         Thanks!</p></>)],
@@ -142,7 +113,7 @@ const solvedMails: (MailData[] | null)[] = [
     // Print Decimal
     null,
     // Calculator // Partner Engineer
-    [createPromotionMessage(Shared.jobTitles[5].minimumSolved, "A special promotion", (self) => <><p>Incredible work, {self.name}! After consulting with the SIC Systems board, I've been given special permission to promote you to {Shared.jobTitles[5].title}.
+    [createPromotionMessage("A special promotion", (self) => <><p>Incredible work, {self.name}! After consulting with the SIC Systems board, I've been given special permission to promote you to {Shared.jobTitles[5].title}.
         </p><p>You've shown tenacity to get this far, and you'll need loads of it for the next batch of tasks. We need to give the SIC-1 the ability to understand its own
         code, in order to unleash its immense computing power on optimizing its own performance. We'll be happy to provide on-site food and laundry service, home
         cleaning/maintenance, and fertility preservation services to you as part of your compensation package. We just need you to push through this one last sprint to
@@ -155,7 +126,7 @@ const solvedMails: (MailData[] | null)[] = [
     // Parse Subleq Instructions
     null,
     // Self-Hosting // Technical Fellow Emeritus
-    [createPromotionMessage(Shared.jobTitles[6].minimumSolved, "Unbelievable work!", (self) => <><p>Truly amazing work, {self.name}! The SIC Systems board unanimously voted to create a new title just for you: {Shared.jobTitles[6].title}.</p><p>Thank you
+    [createPromotionMessage("Unbelievable work!", (self) => <><p>Truly amazing work, {self.name}! The SIC Systems board unanimously voted to create a new title just for you: {Shared.jobTitles[6].title}.</p><p>Thank you
         from the bottom of my heart for all of the sacrifices you've made to get us to this point. The SIC-1 is now able to reason about its own code. This is an
         amazing breakthrough and you should be very proud.</p><p>Now that we've reached this exciting milestone (thanks to your tireless efforts!), SIC Systems honestly can't
         challenge someone with your peerless talent. Excitingly, you can now begin the next phase of your career at one of the many other technology companies around the world.
