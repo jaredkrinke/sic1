9832af3895071a59b555a639b42922783e2769a7
Jared Krinke <jared.krinke@gmail.com>
2022-10-12 17:34:19 -0700

    Refactor mail content into separate files

---
+++ sic1/client/content/dev-environment.tsx
@@ -0,0 +1,38 @@
+export default <>
+<h3>SIC-1 DEVELOPMENT ENVIRONMENT</h3>
+<p>The SIC-1 Development Environment provides a state-of-the-art programming and simulation environment for SIC Systems's Single Instruction Computer Mark 1 (SIC-1).</p>
+<h3>MENUS</h3>
+<p>Menus can be accessed using either the "Esc" key or the "Menu" button in the lower-left of the development environment. The Main Menu provides access to the following:</p>
+<ul>
+    <li><strong>Program Inventory</strong>: Used for switching between assigned programming tasks, as well as viewing employee and task statistics (described in a dedicated section below).</li>
+    <li><strong>Electronic Mail</strong>: An integrated client application for viewing electronic mail from automated agents and other SIC-1 employees (described in a dedicated section below).</li>
+    <li><strong>Options</strong>: A sub-menu for modifying presentation settings (full-screen mode, volume, etc.), and other miscellaneous menus.</li>
+</ul>
+<h3>INTEGRATED DEVELOPMENT ENVIRONMENT</h3>
+<p>The SIC-1 Development Environment desktop is an integrated development environment (IDE) for the SIC-1. There are three main sections (left, right, center):</p>
+<ul>
+    <li><strong>Left</strong>: Current task description, table of input and output (both expected and actual), simulation state/statistics, and buttons for controlling the simulation, and a button for opening the Main Menu (described above).</li>
+    <li><strong>Center</strong>: Main code editor for reading and writing SIC-1 Assembly Language programs (see separate SIC-1 Reference Manual electronic mail for details).</li>
+    <li><strong>Right</strong>: Detailed simulation state, including a complete view of SIC-1 memory, as well as an unprecedented, revolutionary feature: a table of variables and their current values, for convenient lookup.</li>
+</ul>
+<p>Note that during execution the current instruction will be highlighted in the code editor and all inputs/outputs, and memory/variables are visible on the left and right panes.</p>
+<h3>KEYBOARD SHORTCUTS</h3>
+<p>The SIC-1 Development Environment supports the following convenient keyboard shortcuts:</p>
+<ul>
+    <li><strong>Esc</strong>: Open Main Menu or close current menu or pause execution.</li>
+    <li><strong>Ctrl+.</strong>: Execute a single instruction and pause (can also be used during execution to pause).</li>
+    <li><strong>Ctrl+Enter</strong>: Run instructions until completion/error/pause.</li>
+    <li><strong>Ctrl+Shift+Enter</strong>: Halt execution.</li>
+    <li><strong>F11, Alt+Enter</strong>: Toggle full-screen mode.</li>
+</ul>
+<h3>PROGRAM INVENTORY</h3>
+<p>The Program Inventory displays a list of assigned tasks on the left. There is also a special "employee statistics" section at the top for viewing how your progress compares to other SIC Systems employees.</p>
+<p>Select any task on the left to view the description (or your solution's statistics, as compared to other employees' solutions, for completed tasks). There is button at the bottom to load the program into the development environment.</p>
+<h3>ELECTRONIC MAIL</h3>
+<p>SIC Systems is pioneering a new digital form of intra-office communication known as "electronic mail". This eliminates paper waste for memos, and also ensures that all communications are securely routed to only the intended recipients.</p>
+<p>A complete list of received electronic mail is shown in the left pane of the electronic mail viewer (sorted from oldest at the top to newest at the bottom). Note that automated mails are de-emphasized, but they can be opened to view statistics for your completed programming tasks.</p>
+<p>If you have any unread mail, it will appear in a separate section at the top of the left page, for convenience.</p>
+<p>Also, remember that you can always access the SIC-1 Reference Manual and this document about the SIC-1 Development Environment from your employee on-boarding mails at the top (oldest) part of the left pane of the electronic mail viewer.</p>
+<h3>ADDENDUM</h3>
+<p>Although the SIC-1 Development Environment has been thoroughly tested, it is possible that you may encounter unexpected behavior in certain instances. The authors of the SIC-1 Development Environment have assured us that the most likely causes for such issues are either cosmic rays or large solar flares interfering with sensitive electronics in an unpredictable manner. The simplest way to recover from such interference is to unplug the computer, wait for 30 seconds to allow it to cool and eject any rogue electrons, and then plug it back in.</p>
+</>;
