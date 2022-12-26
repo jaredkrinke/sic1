3baae0951d52bd9884de01698a951b141e92f6d0
Jared Krinke <jared.krinke@gmail.com>
2022-12-26 15:12:27 -0800

    Add Steam stats tool

--- sic1/tools/steam/steam-leaderboards.ts
+++ sic1/tools/steam/steam-leaderboards.ts
@@ -1,3 +1,23 @@
+// This is a command line tool for caching Steam leaderboard stats, in histogram form:
+//
+// USAGE: ts-node script.ts <API key> [CSV file]
+
+import { readFile, writeFile } from "fs/promises";
+import { puzzleFlatArray } from "../../shared/puzzles";
+import * as Contract from "../../server/contract/contract";
+
+// TODO: Should be shared with other stats cache tool (or even merged into a single tool)
+type Sic1PuzzleStats = { [puzzleTitle: string]: Contract.PuzzleStatsResponse };
+
+interface Sic1UserStats {
+    solutionsByUser: Contract.HistogramData;
+}
+
+interface Sic1StatsCache {
+    userStats: Sic1UserStats;
+    puzzleStats: Sic1PuzzleStats;
+}
+
 const int32Max = 2147483647;
 
 interface Leaderboard {
@@ -42,7 +62,7 @@ async function getLeaderboardsForGameAsync(key: string, appId: string): Promise<
     return (await getAsync(getLeaderboardsForGame, { key, appid: appId })).response.leaderboards;
 }
 
-async function getLeaderboardEntriesAsync(key: string, appId: string, leaderboardId: number, dataRequest: LeaderboardDataRequest = "global", rangeStart = 0, rangeEnd =  int32Max): Promise<LeaderboardEntry> {
+async function getLeaderboardEntriesAsync(key: string, appId: string, leaderboardId: number, dataRequest: LeaderboardDataRequest = "global", rangeStart = 0, rangeEnd =  int32Max): Promise<LeaderboardEntry[]> {
     return (await getAsync(getLeaderboardEntries, {
         key,
         appid: appId,
@@ -53,12 +73,70 @@ async function getLeaderboardEntriesAsync(key: string, appId: string, leaderboar
     })).leaderboardEntryInformation.leaderboardEntries;
 }
 
-const [ key, leaderboardId ] = Deno.args;
-const appId = await Deno.readTextFile("../../client/windows/steam_appid.txt");
+function convertLeaderboardEntriesToHistogram(entries: LeaderboardEntry[]): Contract.HistogramData {
+    const scoreToCount: { [score: number]: number } = {};
+    for (const { score } of entries) {
+        const previousValue = scoreToCount[score];
+        scoreToCount[score] = previousValue ? (previousValue + 1) : 1;
+    }
 
-// console.log(await getLeaderboardsForGameAsync(key, appId));
-console.log(await getLeaderboardEntriesAsync(key, appId, parseInt(leaderboardId)));
+    return Object.entries(scoreToCount).map(([bucketMax, count]) => ({
+        bucketMax: parseInt(bucketMax),
+        count,
+    }));
+}
 
-// TODO: Try to use Node again, just to keep things consistent...
-// TODO: For userStats and each puzzle, download all leaderboard entries, validate them, bucket, and produce a new stats cache. The two caches can be merged either at build time or run time.
-// TODO: Output a CSV file too!
+function convertHistogramDataToCSV(group: string, metric: string, data: Contract.HistogramData): string {
+    return data.map(bucket => [group, metric, bucket.bucketMax, bucket.count].join(",")).join("\n") + "\n";
+}
+
+const focusMapping = [
+    ["cyclesExecutedBySolution", "cycles"],
+    ["memoryBytesAccessedBySolution", "bytes"],
+];
+
+(async () => {
+    const [ _exePath, _scriptPath, key, csvFile ] = process.argv;
+    const appId = await readFile("../../client/windows/steam_appid.txt", { encoding: "utf8" });
+
+    const leaderboardNameToId: { [name: string]: number } = {};
+    for (const { name, id } of await getLeaderboardsForGameAsync(key, appId)) {
+        leaderboardNameToId[name] = id;
+    }
+
+    // TODO: Validate Steam leaderboard entries
+
+    // User stats
+    const userStats: Sic1UserStats = { solutionsByUser: convertLeaderboardEntriesToHistogram(await getLeaderboardEntriesAsync(key, appId, leaderboardNameToId["Solved Count"])) };
+
+    // Puzzle stats
+    const puzzleStats: Sic1PuzzleStats = {};
+    for (const { title } of puzzleFlatArray) {
+        const response: Partial<Contract.PuzzleStatsResponse> = {};
+        for (const [responseKey, focus] of focusMapping) {
+            const leaderboardName = `${title}_${focus}`;
+            response[responseKey] = convertLeaderboardEntriesToHistogram(await getLeaderboardEntriesAsync(key, appId, leaderboardNameToId[leaderboardName]));
+        }
+        puzzleStats[title] = response as Contract.PuzzleStatsResponse;
+    }
+
+    const statsCache: Sic1StatsCache = {
+        userStats,
+        puzzleStats,
+    };
+
+    console.log(JSON.stringify(statsCache));
+
+    // Output a CSV file, if requested
+    if (csvFile) {
+        let csv = "Group,Metric,Score,Count\n";
+        csv += convertHistogramDataToCSV("Solved Count", "puzzles", userStats.solutionsByUser);
+        for (const { title } of puzzleFlatArray) {
+            for (const [responseKey, focus] of focusMapping) {
+                csv += convertHistogramDataToCSV(title, focus, puzzleStats[title][responseKey]);
+            }
+        }
+
+        await writeFile(csvFile, csv);
+    }
+})();
