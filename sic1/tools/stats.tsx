9835c22ffe377d8b6128d2870a61d424e91a2db1
Jared Krinke <jared.krinke@gmail.com>
2022-12-26 17:14:44 -0800

    Fix stats viewer

--- sic1/tools/stats.tsx
+++ sic1/tools/stats.tsx
@@ -1,26 +1,32 @@
 import { render, Component, ComponentChildren } from "preact";
-import { Puzzle, puzzles as puzzleGroups } from "sic1-shared";
-import { Sic1WebService } from "../client/ts/service";
-import { Sic1DataManager } from "../client/ts/data-manager";
+import { Puzzle, puzzleFlatArray } from "../shared/puzzles";
+import { sortAndNormalizeHistogramData } from "../client/ts/service";
 import { Chart } from "../client/ts/chart";
+import cache from "../client/ts/stats-cache";
+import { HistogramData } from "../server/contract/dist";
+import { ChartData } from "../client/ts/chart-model";
+
+function toChartData(data: HistogramData, bucketCount = 20): ChartData {
+    return {
+        histogram: sortAndNormalizeHistogramData(data, bucketCount),
+        highlightedValue: data[0].bucketMax,
+    };
+}
 
 class Stats extends Component<{ puzzles: Puzzle[] }> {
-    private service: Sic1WebService;
-
     constructor(props) {
         super(props);
-        this.service = new Sic1WebService();
     }
 
     private createPuzzleCharts(puzzle: Puzzle): ComponentChildren {
-        const cycles = 100;
-        const bytes = 50;
-        const promise = this.service.getPuzzleStatsAsync(puzzle.title, cycles, bytes);
-
         return <div className="charts">
-            <Chart title={`Cycles Executed: ${cycles}`} promise={(async () => (await promise).cycles)()} />
-            <Chart title={`Bytes Read: ${bytes}`} promise={(async () => (await promise).bytes)()} />
+            <Chart title={`Cycles Executed`} promise={Promise.resolve(toChartData(cache.puzzleStats[puzzle.title].cyclesExecutedBySolution))} />
+            <Chart title={`Bytes Read`} promise={Promise.resolve(toChartData(cache.puzzleStats[puzzle.title].memoryBytesAccessedBySolution))}  />
         </div>;
+    }
+
+    public componentDidMount(): void {
+        document.documentElement.style.setProperty("font-size", "90%");
     }
 
     public render() {
@@ -28,7 +34,7 @@ class Stats extends Component<{ puzzles: Puzzle[] }> {
             <div>
                 <h3>Users</h3>
                 <div className="charts">
-                    <Chart title={`Completed Tasks`} promise={this.service.getUserStatsAsync()} />
+                    <Chart title={`Completed Tasks`} promise={Promise.resolve(toChartData(cache.userStats.solutionsByUser, 30))} />
                 </div>
             </div>
             {this.props.puzzles.map(puzzle => <div>
@@ -39,4 +45,4 @@ class Stats extends Component<{ puzzles: Puzzle[] }> {
     }
 }
 
-render(<Stats puzzles={[].concat(...puzzleGroups.map(group => group.list)).slice(0, 10)} />, document.getElementById("_stats"));
+render(<Stats puzzles={puzzleFlatArray} />, document.getElementById("_stats"));
