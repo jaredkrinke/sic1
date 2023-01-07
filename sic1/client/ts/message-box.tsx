1e3b972bb6d97efbcde210da4238db585a130bb0
Jared Krinke <jared.krinke@gmail.com>
2023-01-06 21:45:46 -0800

    #297 Keyboard navigation for menu

--- sic1/client/ts/message-box.tsx
+++ sic1/client/ts/message-box.tsx
@@ -3,16 +3,24 @@ import { Button } from "./button";
 
 export interface MessageBoxContent {
     title: string;
+    menu?: boolean;
     modal?: boolean;
     width?: "none" | "wide" | "narrowByDefault";
     body: ComponentChildren;
 }
 
 interface MessageBoxProperties extends MessageBoxContent {
+    previousFocus: Element;
     onDismissed: () => void;
 }
 
 export class MessageBox extends Component<MessageBoxProperties> {
+    private static readonly menuButtonSelector = ".messageBody button";
+    private static readonly keyToOffset = {
+        ArrowUp: -1,
+        ArrowDown: 1,
+    };
+
     constructor(props: MessageBoxProperties) {
         super(props);
     }
@@ -20,6 +28,38 @@ export class MessageBox extends Component<MessageBoxProperties> {
     private close = () => {
         if (this.props.modal !== true) {
             this.props.onDismissed();
+        }
+    }
+
+    private focusButton(offset: number): void {
+        const elements = document.querySelectorAll<HTMLButtonElement>(MessageBox.menuButtonSelector);
+        const activeElement = document.activeElement;
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            if (element === activeElement) {
+                let index = i + offset;
+                if (index < 0) {
+                    index = elements.length - 1;
+                } else if (index >= elements.length) {
+                    index = 0;
+                }
+                
+                elements[index].focus();
+                break;
+            }
+        }
+    }
+
+    public componentDidMount(): void {
+        if (this.props.menu) {
+            document.querySelector<HTMLButtonElement>(MessageBox.menuButtonSelector)?.focus?.();
+        }
+    }
+
+    public componentWillUnmount(): void {
+        const { previousFocus } = this.props;
+        if (previousFocus && (document.activeElement !== previousFocus) && document.body.contains(previousFocus) && previousFocus["focus"]) {
+            previousFocus["focus"]();
         }
     }
 
@@ -32,7 +72,13 @@ export class MessageBox extends Component<MessageBoxProperties> {
                         {this.props.title}
                         {this.props.modal === true ? null : <Button className="messageClose" onClick={this.close} title="Esc">X</Button>}
                     </div>
-                    <div className="messageBody">
+                    <div className="messageBody" onKeyDown={this.props.menu ? (event) => {
+                        const offset = MessageBox.keyToOffset[event.key];
+                        if (offset) {
+                            this.focusButton(offset);
+                            event.preventDefault();
+                        }
+                    } : null}>
                         {this.props.body}
                     </div>
                 </div>
