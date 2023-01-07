06112010fef6f656e4cfbfeae0a5fec3963f89b8
Jared Krinke <jared.krinke@gmail.com>
2023-01-07 11:10:01 -0800

    Consolidate Steam Web API code

---
+++ sic1/tools/cache-stats/steam-api.ts
@@ -0,0 +1,54 @@
+const int32Max = 2147483647;
+
+export interface Leaderboard {
+    id: number;
+    name: string;
+    entries: number;
+    sortmethod: "Ascending" | "Descending";
+}
+
+export interface LeaderboardEntry {
+    steamID: string;
+    score: number;
+    rank: number;
+    ugcid: string;
+    detailData: string;
+}
+
+export const leaderboardDataRequestNames = {
+    global: 0,
+    aroundUser: 1,
+    friends: 2,
+} as const;
+
+export type LeaderboardDataRequest = keyof typeof leaderboardDataRequestNames;
+
+export const getLeaderboardsForGame = "https://partner.steam-api.com/ISteamLeaderboards/GetLeaderboardsForGame/v2/";
+export const getLeaderboardEntries = "https://partner.steam-api.com/ISteamLeaderboards/GetLeaderboardEntries/v1/";
+
+async function getAsync(uri: string, params: Record<string, string>) {
+    const query = `${uri}?${new URLSearchParams(params)}`;
+    console.log(`GET ${query}`);
+
+    const result = await fetch(query, { method: "GET" });
+    if (!result.ok) {
+        throw new Error(`${result.statusText}: ${await result.text()}`);
+    }
+
+    return await result.json();
+}
+
+export async function getLeaderboardsForGameAsync(key: string, appId: string): Promise<Leaderboard[]> {
+    return (await getAsync(getLeaderboardsForGame, { key, appid: appId })).response.leaderboards;
+}
+
+export async function getLeaderboardEntriesAsync(key: string, appId: string, leaderboardId: number, dataRequest: LeaderboardDataRequest = "global", rangeStart = 0, rangeEnd =  int32Max): Promise<LeaderboardEntry[]> {
+    return (await getAsync(getLeaderboardEntries, {
+        key,
+        appid: appId,
+        leaderboardid: leaderboardId.toString(),
+        datarequest: leaderboardDataRequestNames[dataRequest].toString(),
+        rangestart: rangeStart.toString(),
+        rangeend: rangeEnd.toString(),
+    })).leaderboardEntryInformation.leaderboardEntries;
+}
--- sic1/tools/cache-stats/steam-stats.ts
+++ sic1/tools/cache-stats/steam-stats.ts
@@ -6,61 +6,7 @@ import { readFile, writeFile } from "fs/promises";
 import { puzzleFlatArray } from "../../shared/puzzles";
 import * as Contract from "../../server/contract/contract";
 import type { Sic1PuzzleStats, Sic1StatsCache } from "../../client/ts/stats-cache";
-
-const int32Max = 2147483647;
-
-interface Leaderboard {
-    id: number;
-    name: string;
-    entries: number;
-    sortmethod: "Ascending" | "Descending";
-}
-
-interface LeaderboardEntry {
-    steamID: string;
-    score: number;
-    rank: number;
-    ugcid: string;
-    detailData: string;
-}
-
-const leaderboardDataRequestNames = {
-    global: 0,
-    aroundUser: 1,
-    friends: 2,
-} as const;
-
-type LeaderboardDataRequest = keyof typeof leaderboardDataRequestNames;
-
-const getLeaderboardsForGame = "https://partner.steam-api.com/ISteamLeaderboards/GetLeaderboardsForGame/v2/";
-const getLeaderboardEntries = "https://partner.steam-api.com/ISteamLeaderboards/GetLeaderboardEntries/v1/";
-
-async function getAsync(uri: string, params: Record<string, string>) {
-    const query = `${uri}?${new URLSearchParams(params)}`;
-    console.log(`GET ${query}`);
-
-    const result = await fetch(query, { method: "GET" });
-    if (!result.ok) {
-        throw new Error(`${result.statusText}: ${await result.text()}`);
-    }
-
-    return await result.json();
-}
-
-async function getLeaderboardsForGameAsync(key: string, appId: string): Promise<Leaderboard[]> {
-    return (await getAsync(getLeaderboardsForGame, { key, appid: appId })).response.leaderboards;
-}
-
-async function getLeaderboardEntriesAsync(key: string, appId: string, leaderboardId: number, dataRequest: LeaderboardDataRequest = "global", rangeStart = 0, rangeEnd =  int32Max): Promise<LeaderboardEntry[]> {
-    return (await getAsync(getLeaderboardEntries, {
-        key,
-        appid: appId,
-        leaderboardid: leaderboardId.toString(),
-        datarequest: leaderboardDataRequestNames[dataRequest].toString(),
-        rangestart: rangeStart.toString(),
-        rangeend: rangeEnd.toString(),
-    })).leaderboardEntryInformation.leaderboardEntries;
-}
+import { getLeaderboardEntriesAsync, getLeaderboardsForGameAsync, LeaderboardEntry } from "./steam-api";
 
 function convertLeaderboardEntriesToHistogram(entries: LeaderboardEntry[]): Contract.HistogramData {
     const scoreToCount: { [score: number]: number } = {};
