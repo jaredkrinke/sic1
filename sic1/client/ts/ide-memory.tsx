e9fe0ff07a26f8084c07281fdd367912502e2987
Jared Krinke <jared.krinke@gmail.com>
2023-01-25 17:13:16 -0800

    #281 Click to highlight memory cell

--- sic1/client/ts/ide-memory.tsx
+++ sic1/client/ts/ide-memory.tsx
@@ -7,26 +7,31 @@ export interface Sic1MemoryProps {
     currentAddress: number | null;
     memoryMap: number[][];
     memory: { [address: number]: number };
+    watchedAddresses: Set<number>;
     highlightAddress?: number;
 
     onSetHighlightAddress: (address?: number) => void;
+    onToggleWatch: (address: number) => void;
 }
 
 export class Sic1Memory extends Component<Sic1MemoryProps> {
     public render(): ComponentChild {
-        return <table className="memory"><tr><th colSpan={16}>Memory</th></tr>
+        return <table className={`memory${this.props.hasStarted ? " running" : ""}`}><tr><th colSpan={16}>Memory</th></tr>
         {
             this.props.memoryMap.map(row => <tr>{row.map(index =>
-                <td className={
+                <td className={`${
                     (this.props.hasStarted && (this.props.highlightAddress === index))
                         ? "attention"
                         : ((this.props.currentAddress !== null && index >= this.props.currentAddress && index < this.props.currentAddress + Constants.subleqInstructionBytes)
                             ? "emphasize"
                             : "")
-                }
+                }${
+                    this.props.watchedAddresses.has(index) ? " watched" : ""
+                }`}
 
                     onMouseEnter={() => this.props.onSetHighlightAddress(index)}
                     onMouseLeave={() => this.props.onSetHighlightAddress(undefined)}
+                    onClick={this.props.hasStarted ? () => this.props.onToggleWatch(index) : undefined}
                 >
                     <NumberSpan format="hex" value={this.props.memory[index]} />
                 </td>)}</tr>)
