4308f04f98392e6c804aa43eef74e81b242685e3
Jared Krinke <jared.krinke@gmail.com>
2023-01-28 16:04:25 -0800

    Support iteration count argument

--- sic1/tools/cli/verify-solution.ts
+++ sic1/tools/cli/verify-solution.ts
@@ -1,10 +1,15 @@
 import { puzzleFlatArray, solutionBytesMax, verifySolution } from "../../shared/puzzles";
 import { unhexifyBytes } from "./shared";
 
-const [ _host, _script, puzzleName, program ] = process.argv;
+const [ _host, _script, puzzleName, program, iterationsRaw ] = process.argv;
+
+const iterations = iterationsRaw ?? 1;
 
 try {
-    verifySolution(puzzleFlatArray.find(p => p.title === puzzleName), unhexifyBytes(program), 10000, solutionBytesMax);
+    for (let i = 0; i < iterations; i++) {
+        verifySolution(puzzleFlatArray.find(p => p.title === puzzleName), unhexifyBytes(program), 10000, solutionBytesMax);
+    }
+
     console.log("Valid!");
 } catch (error) {
     console.log(`*** Invalid: ${error.message}`);
