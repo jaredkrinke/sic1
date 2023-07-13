495c9ac9b7853030302078e684dc89e1f23ae305
Jared Krinke <jared.krinke@gmail.com>
2023-07-13 16:02:12 -0700

    Untested implementation

--- sic1/client/c-steam-api/c-steam-api.h
+++ sic1/client/c-steam-api/c-steam-api.h
@@ -6,22 +6,21 @@
 #define C_STEAM_API extern "C" __declspec(dllimport) int
 
 // Startup and shutdown
-C_STEAM_API c_steam_start(unsigned int app_id, int* should_restart);
+C_STEAM_API c_steam_start(unsigned int app_id, int* out_should_restart);
 C_STEAM_API c_steam_stop();
 
 // Use this to free allocated strings returned from the below functions
 C_STEAM_API c_steam_free_string(char* data);
 
 // User info
-C_STEAM_API c_steam_user_name_get(char** user_name);
+C_STEAM_API c_steam_user_name_get(char** out_user_name);
 
 // Leaderboards
-C_STEAM_API c_steam_leaderboard_get(const char* leaderboard_name, void** leaderboard);
-C_STEAM_API c_steam_leaderboard_free(void* leaderboard);
-C_STEAM_API c_steam_leaderboard_set_score(void* leaderboard, int score, const unsigned char* detail, size_t detail_bytes);
-C_STEAM_API c_steam_leaderboard_get_friend_scores(void* leaderboard, char** friend_scores_json);
+C_STEAM_API c_steam_leaderboard_get(const char* leaderboard_name, unsigned long long* out_leaderboard);
+C_STEAM_API c_steam_leaderboard_set_score(unsigned long long leaderboard, int score, const int* detail, int detail_count, int* out_score_changed);
+C_STEAM_API c_steam_leaderboard_get_friend_scores(unsigned long long leaderboard, char** out_friend_scores_json);
 
 // Achievements
-C_STEAM_API c_steam_achievement_get(const char* achievement_id, int* achieved);
-C_STEAM_API c_steam_achievement_set(const char* achievement_id, int* newly_achieved);
+C_STEAM_API c_steam_achievement_get(const char* achievement_id, int* out_achieved);
+C_STEAM_API c_steam_achievement_set(const char* achievement_id, int* out_newly_achieved);
 C_STEAM_API c_steam_achivements_store();
---
+++ sic1/client/c-steam-api/steam_call_manager.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// TODO: This dependency should be removed if I ever need to support another platform or want to release this DLL separately
+#include "../windows/steamcallmanager.h"
