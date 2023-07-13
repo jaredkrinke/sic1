f6c94cb1de852f6a6e3f8c0c4bd0c9c56b557bba
Jared Krinke <jared.krinke@gmail.com>
2023-07-13 13:58:26 -0700

    Old Electron experiment

---
+++ sic1/client/electron/src/preload.js
@@ -0,0 +1,7 @@
+const greenworks = require("greenworks");
+
+if (greenworks.init()) {
+    // TODO: This should translate between SIC-1's WebView2-based `Platform` abstraction and Greenworks
+} else {
+    // TODO: Abort?
+}
--- sic1/client/electron/src/start.js
+++ sic1/client/electron/src/start.js
@@ -1,19 +1,28 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require("path");
 
 // TODO: Why was this in the template?
 app.allowRendererProcessReuse = false;
 
 app.on('window-all-closed', function() {
-    if (process.platform != 'darwin')
+    if (process.platform != 'darwin') {
         app.quit();
+    }
 });
 
 app.on('ready', function() {
     const mainWindow = new BrowserWindow({
         width: 1600,
         height: 1200,
-        webPreferences: { nodeIntegration: true },
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.join(__dirname, "preload.js"),
+        },
     });
+
     mainWindow.webContents.openDevTools();
     mainWindow.loadURL('file://' + __dirname + '/content/index.html');
+
+    // TODO: Register "activate" handler and create a window if needed for macOS?
 });
