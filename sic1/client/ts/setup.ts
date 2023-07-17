7a6c6877847370ba026f74d54a63d33e926129ec
Jared Krinke <jared.krinke@gmail.com>
2023-07-17 15:33:44 -0700

    Upload JS errors under Electron

--- sic1/client/ts/setup.ts
+++ sic1/client/ts/setup.ts
@@ -1,6 +1,7 @@
 import * as backtrace from "backtrace-js";
 
 export type PlatformName = "steam" | "web";
+export type RuntimeName = "browser" | "electron" | "webview2";
 
 // Check for debug mode:
 //
@@ -18,16 +19,19 @@ export const debug = (debugSearchParameter === null)
 export const suppressUpload = (url.searchParams.get("upload") === "0");
 
 // Determine platform (if chrome.webview.hostObjects.sync.steam exists, assume "steam"; otherwise "web")
-export const platform: PlatformName = ((window as any).chrome?.webview?.hostObjects?.sync?.steam) ? "steam" : "web";
+const steamHostObject = ((window as any).chrome?.webview?.hostObjects?.sync?.steam);
+export const platform: PlatformName = steamHostObject ? "steam" : "web";
+export const runtime: RuntimeName = steamHostObject ? (steamHostObject.Runtime ?? "webview2") : "browser";
 
 if (debug) {
     // Pop up a dialog in debug mode for unhandled errors. Unhandled promise rejections are logged to the debug console.
     window.onerror = (msg: string | Event, url, lineNumber, columnNumber, error) => alert(JSON.stringify({ msg, url, lineNumber, columnNumber, error }, undefined, 4));
 } else {
     // Use Backtrace.io for error reporting when *not* in debug mode
-    const platformToToken = {
-        steam: "de4bca94436fd7879be4a36d9a689358f274387341b06722b838c5f10f921790",
-        web: "3d0813b41068cb04f05b582a7e19ddc821bd3e583cc625f80f6046ab4e5f42ed",
+    const runtimeToToken = {
+        browser: "3d0813b41068cb04f05b582a7e19ddc821bd3e583cc625f80f6046ab4e5f42ed",
+        electron: "a997d4dc205879a2ab25fe2ccdc542e14fbe3fa455426dde57890a7549c3a075",
+        webview2: "de4bca94436fd7879be4a36d9a689358f274387341b06722b838c5f10f921790",
     };
 
     // For app mode, exit the app if an error occurs (after reporting the error via Backtrace)
@@ -53,7 +57,7 @@ if (debug) {
 
     try {
         backtrace.initialize({
-            endpoint: `https://submit.backtrace.io/apg/${platformToToken[platform]}/json`,
+            endpoint: `https://submit.backtrace.io/apg/${runtimeToToken[runtime]}/json`,
             disableGlobalHandler: closeOnUnhandledError,
             handlePromises: true,
         });
