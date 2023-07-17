049a5dc468b884d58d92a4ea35eb87deb075ad91
Jared Krinke <jared.krinke@gmail.com>
2023-07-17 15:49:30 -0700

    Add Electron-related licenses

--- sic1/client/ts/licenses.ts
+++ sic1/client/ts/licenses.ts
@@ -349,4 +349,39 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
+===========================================================
+nlohmann.json
+===========================================================
+
+MIT License 
+
+Copyright (c) 2013-2022 Niels Lohmann
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+===========================================================
+Electron
+===========================================================
+
+The web and primary Steam releases are not based on Electron, but there is a
+port of SIC-1 to Electron. To view the licenses for Electron, open the
+"LICENSE" and "LICENSES.chromium.html" files that are included in the root of
+that binary distribution, e.g. "Steam/steamapps/common/SIC-1/LICENSE".
+
 `;
