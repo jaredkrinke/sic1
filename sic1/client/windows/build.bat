faad2d1ca65a406d6ed15ef1f734db65973f752c
Jared Krinke <jared.krinke@gmail.com>
2023-07-24 22:25:11 -0700

    #425 Build for Linux

--- sic1/client/windows/build.bat
+++ sic1/client/windows/build.bat
@@ -1,6 +1,7 @@
 rem Extract client version from package.json
 for /f %%i in ('node -e "process.stdout.write(require(`../package.json`).version)"') do set SIC1_VERSION=%%i
 
+rem Build the web game
 pushd ..
 rd /s /q dist
 rd /s /q .parcel-cache
@@ -8,10 +9,17 @@ call npm run build:mail
 call npm run build
 popd
 
+rem Build for Steam on Linux (based on Electron)
+pushd ..\electron
+call npm run make -- --platform linux
+popd
+
+rem Include WebView redistributable
 mkdir content\shared
 copy /y redist\MicrosoftEdgeWebview2Setup.exe content\shared
 copy /y webview2.vdf content\shared
 
+rem Build for Steam on Windows (based on WebView2)
 rd /s /q Debug
 rd /s /q Release
 msbuild sic1.sln /p:Configuration=Release /t:rebuild /p:Platform=x86 /p:Configuration=Release
@@ -33,6 +41,7 @@ copy /y x64\Release\sic1.exe content\64bit
 copy /y x64\Release\steam_api64.dll content\64bit
 copy /y x64\Release\WebView2Loader.dll content\64bit
 
+rem Save symbols for native code
 rd /s /q symbols
 mkdir symbols\32bit
 copy /y Release\*.pdb symbols\32bit
@@ -41,4 +50,6 @@ copy /y x64\Release\*.pdb symbols\64bit
 mkdir symbols\shared
 copy /y ..\dist\*.map symbols\shared
 
+rem Archive build
+rem Note: I'm not archiving the Linux builds since nothing actually gets compiled
 powershell -Command "$timestamp = (Get-Date).ToUniversalTime().ToString('yyyy-MM-ddTHH_mm_ssZ'); Compress-Archive -Path content,symbols -DestinationPath artifacts\sic1-$Env:SIC1_VERSION-$timestamp.zip; Compress-Archive -Path content\32bit\assets\* -DestinationPath artifacts\sic1-web-$Env:SIC1_VERSION.zip;"
