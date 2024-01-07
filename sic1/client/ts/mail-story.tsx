c7b365a2ce184cb6734fd7b37b930e20a512cfab
Jared Krinke <jared.krinke@gmail.com>
2024-01-07 09:12:47 -0800

    #512 Localize dev env manual

--- sic1/client/ts/mail-story.tsx
+++ sic1/client/ts/mail-story.tsx
@@ -7,6 +7,78 @@ import { Shared } from "./shared";
 export const storyMailContents: Mail[] = [
 
     {
+        id: "s0_1",
+        from: Contacts.onboarding,
+        subject: <FormattedMessage
+            id="mails0_1Subject"
+            description="Subject line for story mail s0_1"
+            defaultMessage="SIC-1 Dev. Environment"
+            />,
+        create: (context) => <FormattedMessage
+            id="mails0_1Content"
+            description="HTML content for story mail s0_1"
+            defaultMessage={`<h3>SIC-1 DEVELOPMENT ENVIRONMENT</h3>
+<p>The SIC-1 Development Environment provides a state-of-the-art programming and simulation environment for SIC Systems's Single Instruction Computer Mark 1 (SIC-1).</p>
+<h3>MENUS</h3>
+<p>Menus can be accessed using either the "Esc" key or the "Menu" button in the lower-left of the development environment. The Main Menu provides access to the following:</p>
+<ul>
+<li><strong>Program Inventory</strong>: Used for switching between assigned programming tasks, as well as viewing employee and task statistics (described in a dedicated section below).</li>
+<li><strong>Electronic Mail</strong>: An integrated client application for viewing electronic mail from automated agents and other SIC-1 employees (described in a dedicated section below).</li>
+<li><strong>Options</strong>: A sub-menu for modifying presentation settings (full-screen mode, volume, etc.), and other miscellaneous menus.</li>
+</ul>
+<h3>INTEGRATED DEVELOPMENT ENVIRONMENT</h3>
+<p>The SIC-1 Development Environment desktop is an integrated development environment (IDE) for the SIC-1. There are three main sections (left, right, center):</p>
+<ul>
+<li><strong>Left</strong>: Current task description, table of input and output (both expected and actual), simulation state/statistics, buttons for controlling the simulation, and a button for opening the Main Menu (described above).</li>
+<li><strong>Center</strong>: Main code editor for reading and writing SIC-1 Assembly Language programs (see the separate SIC-1 Reference Manual electronic mail for details).</li>
+<li><strong>Right</strong>: Detailed simulation state including a complete view of SIC-1 memory, code manipulation tools, as well as an unprecedented, revolutionary feature: a table of variables and their current values, for convenient lookup.</li>
+</ul>
+<p>The memory table is shown in hexadecimal for compactness; hover over a cell to see the corresponding decimal value.</p>
+<p>During execution, the current instruction will be highlighted in both the code editor (center) and the memory table (upper right), current inputs and outputs are highlighted in the tables on the left, and variables are displayed in a table on the right (hover for hexadecimal and unsigned representations, if needed).</p>
+<p>To aid debugging, it is possible to set breakpoints on <code>subleq</code> instructions. When hit, these breakpoints will pause execution for manual analysis. There are two ways to set breakpoints:</p>
+<ul>
+<li>In code, add <code>!</code> to the beginning of a line (e.g. <code>!subleq @OUT, @IN</code>).</li>
+<li>During execution, click the small circle to the left of any <code>subleq</code> instruction to toggle the breakpoint.</li>
+</ul>
+<p>Note that each program may be tested using multiple distinct test sets, the state of the SIC-1 resets between test sets, and the test sets generally include randomly generated input data. Solutions should be robust to arbitrary random data, in order to be eligible for inclusion in solution statistics.</p>
+<h3>KEYBOARD SHORTCUTS</h3>
+<p>The SIC-1 Development Environment supports the following convenient keyboard shortcuts:</p>
+<ul>
+<li><strong>Esc</strong>: Open/close menu or, if running, pause/halt execution.</li>
+<li><strong>Ctrl+.</strong>: Execute a single instruction and pause. If the program has not been compiled yet, this will compile the program and pause before executing the first instruction.</li>
+<li><strong>Ctrl+Enter</strong>: Run instructions until completion/error/pause. If already running, this will increase the speed of execution.</li>
+<li><strong>Ctrl+Shift+Enter</strong>: Pause execution (if running), otherwise halt execution.</li>
+<li><strong>F11, Alt+Enter</strong>: Toggle full-screen mode.</li>
+</ul>
+<p>Code editing shortcuts:</p>
+<ul>
+<li><strong>Ctrl+K Ctrl+F</strong>: Comment out all of the selected lines</li>
+<li><strong>Ctrl+K Ctrl+U</strong>: Uncomment all of the selected lines</li>
+<li><strong>Ctrl+M</strong>: Toggle <strong>Tab Insert</strong> mode (by default the Tab key cycles focus, with Tab Insert mode, tabs can be entered into the code editor)</li>
+<li><strong>Tab</strong>: If in <strong>Tab Insert</strong> mode, insert tabs into the selected lines</li>
+<li><strong>Shift+Tab</strong>: If in <strong>Tab Insert</strong> mode, un-indent the selected lines</li>
+</ul>
+<h3>PROGRAM INVENTORY</h3>
+<p>The Program Inventory displays a list of assigned tasks on the left. There is also a special "employee statistics" section at the top for viewing how your progress compares to other SIC Systems employees.</p>
+<p>Select any task on the left to view the description (or your solution's statistics, as compared to other employees' solutions, for completed tasks). There is button at the bottom to load the program into the development environment.</p>
+<p>Under the "File Selection" heading, it's possible to manage multiple implementations of a task (notably: "New" creates a new solution with default contents, "Copy" duplicates an existing implementation). This can be useful when there is one solution optimizing for fewest cycles, and a different solution optimizing for accessing the least amount of memory.</p>
+<p>If available, other non-task entries will appear in the Program Inventory, under the "Diversions" heading (such as Sandbox Mode or any company-sanctioned electronic video games--only for use during legally-mandated breaks).</p>
+<h3>ELECTRONIC MAIL</h3>
+<p>SIC Systems is pioneering a new digital form of intra-office communication known as "electronic mail". This eliminates paper waste for memos, and also ensures that all communications are securely routed to only the intended recipients.</p>
+<p>A complete list of received electronic mail is shown in the left pane of the electronic mail viewer (sorted from oldest at the top to newest at the bottom). Note that automated mails are de-emphasized, but they can be opened to view statistics for your completed programming tasks.</p>
+<p>If you have any unread mail, it will appear in a separate section at the top of the left page, for convenience.</p>
+<p>Also, remember that you can always access the SIC-1 Reference Manual and this document about the SIC-1 Development Environment from your employee on-boarding mails at the top (oldest) part of the left pane of the electronic mail viewer.</p>
+<h3>ADDENDUM</h3>
+<p>Although the SIC-1 Development Environment has been thoroughly tested, it is possible that you may encounter unexpected behavior in certain instances. The authors of the SIC-1 Development Environment have assured us that the most likely causes for such issues are either cosmic rays or large solar flares interfering with sensitive electronics in an unpredictable manner. The simplest way to recover from such interference is to unplug the computer, wait for 30 seconds to allow it to cool and eject any rogue electrons, and then plug it back in.</p>
+`}
+            />,
+        actions: [
+            "manualInNewWindow"
+        ],
+    },
+
+
+    {
         id: "s0_2",
         from: Contacts.hr,
         subject: <FormattedMessage
