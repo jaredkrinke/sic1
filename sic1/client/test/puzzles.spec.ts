04c16b335ee7467a47b658025c2b73faba7b2fee
Jared Krinke <jared.krinke@gmail.com>
2024-01-15 21:19:58 -0800

    Fix puzzle tests

--- sic1/client/test/puzzles.spec.ts
+++ sic1/client/test/puzzles.spec.ts
@@ -1,36 +1,34 @@
 import "mocha";
-import { puzzles, shuffleInPlace } from "../../shared/puzzles";
+import { puzzleFlatArray, shuffleInPlace } from "../../shared/puzzles";
 import * as assert from "assert";
 
 describe("Random test validators", () => {
-    puzzles.forEach(group => { describe(group.groupTitle, () => {
-        group.list.forEach(puzzle => { it(puzzle.title, () => {
-            if (puzzle.test) {
-                const inputs = [];
-                const outputs = [];
-                puzzle.io.forEach(row => {
-                    const input = row[0];
-                    const output = row[1];
-                    inputs.push(input);
-                    outputs.push(output);
-                });
+    puzzleFlatArray.forEach(puzzle => { it(puzzle.title, () => {
+        if (puzzle.test) {
+            const inputs = [];
+            const outputs = [];
+            puzzle.io.forEach(row => {
+                const input = row[0];
+                const output = row[1];
+                inputs.push(input);
+                outputs.push(output);
+            });
 
-                assert.deepStrictEqual(outputs, puzzle.test.getExpectedOutput(inputs));
+            assert.deepStrictEqual(outputs, puzzle.test.getExpectedOutput(inputs));
 
-                let randomInputSequence = puzzle.test.createRandomTest();
-                if (puzzle.test.fixed) {
-                    for (const fixed of puzzle.test.fixed) {
-                        randomInputSequence = randomInputSequence.concat(fixed);
-                    }
-                }
-
-                shuffleInPlace(randomInputSequence);
-
-                const expectedOutputSequence = puzzle.test.getExpectedOutput(randomInputSequence);
-                for (let i = 0; i < randomInputSequence.length; i++) {
-                    // console.log(`[${randomInputSequence[i].join(", ")}] => [${expectedOutputSequence[i].join(", ")}]`);
+            let randomInputSequence = puzzle.test.createRandomTest();
+            if (puzzle.test.fixed) {
+                for (const fixed of puzzle.test.fixed) {
+                    randomInputSequence = randomInputSequence.concat(fixed);
                 }
             }
-        });});
-    });});
+
+            shuffleInPlace(randomInputSequence);
+
+            const expectedOutputSequence = puzzle.test.getExpectedOutput(randomInputSequence);
+            for (let i = 0; i < randomInputSequence.length; i++) {
+                // console.log(`[${randomInputSequence[i].join(", ")}] => [${expectedOutputSequence[i].join(", ")}]`);
+            }
+        }
+    }); });
 });
