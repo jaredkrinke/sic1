fb4fd20e2bd2bcee786a401502841b28dde05521
Jared Krinke <jared.krinke@gmail.com>
2024-01-23 21:21:46 -0800

    Fix keyboard event type

--- sic1/client/ts/avoision.tsx
+++ sic1/client/ts/avoision.tsx
@@ -297,7 +297,7 @@ export class Avoision extends React.Component<AvoisionProps> {
         }
     }
 
-    private onKeyEvent(event: KeyboardEvent, down: boolean): void {
+    private onKeyEvent(event: React.KeyboardEvent<HTMLCanvasElement>, down: boolean): void {
         const control = keyCodeToControl[event.code];
         if (control) {
             this.controlStates[control] = down;
